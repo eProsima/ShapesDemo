52d636eadac67d3d0499fc1f670f698f5abaafae
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2017-07-04 12:26:21 +0200

    Refs #1869. License APACHE 2.0

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,10 +1,17 @@
-/*************************************************************************
- * Copyright (c) 2014 eProsima. All rights reserved.
- *
- * This copy of eProsima Fast RTPS ShapesDemo is licensed to you under the terms described in the
- * FASTRTPS_LIBRARY_LICENSE file included in this distribution.
- *
- *************************************************************************/
+// Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <eprosimashapesdemo/qt/mainwindow.h>
 #include <eprosimashapesdemo/qt/publishdialog.h>
 #include <eprosimashapesdemo/qt/subscribedialog.h>
@@ -369,7 +376,7 @@ void MainWindow::on_actionAbout_triggered()
     msgBox.setWindowTitle("About");
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.setTextFormat(Qt::RichText);   //this is what makes the links clickable
-    msgBox.setText(QString("<p><strong>eProsima Shapes Demo 1.3.1</strong></p><p>Copyright (C) eProsima 2016</p> <p><a href=http://www.eProsima.com>http:://www.eProsima.com</a></p>"));
+    msgBox.setText(QString("<p><strong>eProsima Shapes Demo 1.3.1</strong></p><p>Copyright (C) eProsima 2017</p> <p><a href=http://www.eProsima.com>http:://www.eProsima.com</a></p>"));
     msgBox.exec();
 
 }
