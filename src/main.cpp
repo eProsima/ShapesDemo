dc52d15ece2b6ad6e1851367f90b665c1c022f31
Ricardo González Moreno <correoricky@gmail.com>
2018-10-25 12:31:58 +0200

    Updated how thirdparty are loaded.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <asio.hpp>
 #include <eprosimashapesdemo/qt/mainwindow.h>
 #include <fastrtps/log/Log.h>
 #include <fastrtps/utils/eClock.h>
