932d7ed6e3d968408a1ef6242d4bb1f72167feff
laura <52071480+laura-eprosima@users.noreply.github.com>
2019-09-12 11:05:17 +0200

    Feature/remove eclock (#22)
    * Removing eClock from ShapesDemo
    
    * Change Windows LPSTR by char*
    
    * Remove eClock include
    
    * Update Fast-RTPS to current commit

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,16 @@
 
 #include <eprosimashapesdemo/qt/mainwindow.h>
 #include <fastrtps/log/Log.h>
-#include <fastrtps/utils/eClock.h>
 #include <QApplication>
 #include <eprosimashapesdemo/utils/md5.h>
 #include <fastrtps/Domain.h>
 
 #ifdef WIN32
-int CALLBACK WinMain(
-    _In_ HINSTANCE /*hInstance*/,
-    _In_ HINSTANCE /*hPrevInstance*/,
-    _In_ LPSTR     /*lpCmdLine*/,
-    _In_ int       /*nCmdShow*/
+int __stdcall WinMain(
+    _In_ HINSTANCE   /*hInstance*/,
+    _In_ HINSTANCE   /*hPrevInstance*/,
+    _In_ char* /*lpCmdLine*/,
+    _In_ int         /*nCmdShow*/
 )
 {
     int argc = 0;
