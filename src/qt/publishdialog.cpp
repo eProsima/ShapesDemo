b9ab8698707987e14b2d718c317f89345f79b229
elianalf <62831776+elianalf@users.noreply.github.com>
2023-12-11 15:05:31 +0100

    Refs 20106: Add checkbox to configure data representation
    Signed-off-by: elianalf <62831776+elianalf@users.noreply.github.com>

--- src/qt/publishdialog.cpp
+++ src/qt/publishdialog.cpp
@@ -158,6 +158,21 @@ void PublishDialog::on_button_OkCancel_accepted()
         case 1: SP->m_dw_qos.durability().kind = eprosima::fastdds::dds::TRANSIENT_LOCAL_DURABILITY_QOS; break;
     }
 
+    std::cout<<"Create Shape publisher"<<std::endl;
+    //DATA REPRESENTATION
+    switch (this->ui->comboBox_representation->currentIndex())
+    {
+        case 0: {SP->m_dw_qos.representation().m_value.push_back(eprosima::fastdds::dds::XCDR_DATA_REPRESENTATION);
+                std::cout<<"case 0"<<std::endl;
+                 }
+        break;
+        case 1: { SP->m_dw_qos.representation().m_value.push_back(eprosima::fastdds::dds::XCDR2_DATA_REPRESENTATION);
+                std::cout<<"caso 1"<<std::endl;
+                }
+        break;
+    }
+
+
     //OWNERSHIP:
     switch (this->ui->comboBox_ownership->currentIndex())
     {
@@ -305,7 +320,7 @@ void PublishDialog::on_comboBox_ownership_currentIndexChanged(
 
             break;
         }
-        case 1: 
+        case 1:
         {
             this->ui->checkBox_reliable->setChecked(true);
 
