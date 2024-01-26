51eb2731209740158a74a5e2f7f2f3a308b9626e
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2024-01-26 07:55:58 +0100

    Add an auto fill type information check (#107)
    * Refs #20306: Add autofill type information check
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #20306: Apply Miguel's suggestions
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    ---------
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    (cherry picked from commit a002fdb1567dc50e2bc54f0893fe709169e1328b)
    
    # Conflicts:
    #	forms/participantdialog.ui
    #	include/eprosimashapesdemo/qt/participantdialog.h
    #	include/eprosimashapesdemo/shapesdemo/ShapesDemo.h
    #	src/qt/participantdialog.cpp

--- include/eprosimashapesdemo/shapesdemo/ShapesDemo.h
+++ include/eprosimashapesdemo/shapesdemo/ShapesDemo.h
@@ -71,6 +71,13 @@ public:
     uint32_t m_updateIntervalMs;
     uint32_t m_movementSpeed;
     uint32_t m_domainId;
+<<<<<<< HEAD
+=======
+    uint32_t m_lossPerc;
+    bool m_lossSampleEnabled;
+    bool m_auto_fill_type_information;
+
+>>>>>>> a002fdb (Add an auto fill type information check (#107))
     ShapesDemoOptions()
     {
         m_udp_transport = true;
@@ -86,6 +93,14 @@ public:
         m_movementSpeed = 7;
         m_domainId = 0;
         m_tcp_type = QString("TCP LAN Server");
+<<<<<<< HEAD
+=======
+        m_monitor_service = true;
+        m_lossPerc = 1;
+        m_lossSampleEnabled = false;
+        m_auto_fill_type_information = true;
+
+>>>>>>> a002fdb (Add an auto fill type information check (#107))
 #ifdef ENABLE_ROS_COMPONENTS
         m_ros2_topic = detect_ros_2_installation();
 #else
