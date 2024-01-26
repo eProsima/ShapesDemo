540a9e72049472ac9d1738aed1fde9254450775b
JesusPoderoso <jesuspoderoso@eprosima.com>
2024-01-26 09:19:02 +0100

    Refs #20031: Fix conflicts
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- src/qt/participantdialog.cpp
+++ src/qt/participantdialog.cpp
@@ -58,13 +58,7 @@ ParticipantDialog::ParticipantDialog(
     this->ui->rosTopicCheckBox->setVisible(false);
 #endif // ifdef ENABLE_ROS_COMPONENTS
 
-<<<<<<< HEAD
-=======
     this->ui->typeinformationCheckBox->setChecked(m_options->m_auto_fill_type_information);
-
-    // Percentage loss Configuration
-    this->ui->lossSpin->setValue(m_options->m_lossPerc);
->>>>>>> a002fdb (Add an auto fill type information check (#107))
 
     setEnableState();
     setAttribute ( Qt::WA_DeleteOnClose, true );
@@ -99,13 +93,7 @@ void ParticipantDialog::setEnableState()
     this->ui->SHMcheckBox->setEnabled(mb_started);
     this->ui->UDPcheckBox->setEnabled(mb_started);
     this->ui->TCPcheckBox->setEnabled(mb_started);
-<<<<<<< HEAD
-=======
     this->ui->typeinformationCheckBox->setEnabled(mb_started);
-    this->ui->lossCheckBox->setEnabled(mb_started);
-    this->ui->lossSpin->setEnabled(this->ui->lossCheckBox->isChecked());
-    this->ui->label_7->setEnabled(this->ui->lossCheckBox->isChecked());
->>>>>>> a002fdb (Add an auto fill type information check (#107))
 
     // Enable in Running
     this->ui->pushButton_stop->setEnabled(!mb_started);
@@ -265,22 +253,6 @@ void ParticipantDialog::on_TCPcomboBox_currentTextChanged(
 
     tcp_enable_buttons();
 }
-<<<<<<< HEAD
-=======
-
-void ParticipantDialog::on_monitorServiceCheckBox_stateChanged(
-        int arg1)
-{
-    m_options->m_monitor_service = arg1;
-    mp_sd->setOptions(*m_options);
-}
-
-void ParticipantDialog::on_lossSpin_valueChanged(
-        int arg1)
-{
-    m_options->m_lossPerc = arg1;
-    mp_sd->setOptions(*m_options);
-}
 
 void ParticipantDialog::on_typeinformationCheckBox_stateChanged(
         int arg1)
@@ -288,77 +260,3 @@ void ParticipantDialog::on_typeinformationCheckBox_stateChanged(
     m_options->m_auto_fill_type_information = arg1;
     mp_sd->setOptions(*m_options);
 }
-
-void ParticipantDialog::on_lossCheckBox_stateChanged(
-        int arg1)
-{
-    if (arg1)
-    {
-        QMessageBox msgBox;
-        msgBox.setWindowTitle("Warning");
-        msgBox.setText("Enabling this modality results in sample loss! Do you want to continue?");
-        msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
-        msgBox.setDefaultButton(QMessageBox::Cancel);
-        int out_ = msgBox.exec();
-        switch (out_){
-            case QMessageBox::Ok:
-            {
-                // Transport Configurations
-                this->ui->IntraprocesscheckBox->setEnabled(false);
-                this->ui->DataSharingcheckBox->setEnabled(false);
-                this->ui->SHMcheckBox->setEnabled(false);
-                this->ui->UDPcheckBox->setEnabled(false);
-                this->ui->TCPcheckBox->setEnabled(false);
-                this->ui->IntraprocesscheckBox->setChecked(false);
-                this->ui->DataSharingcheckBox->setChecked(false);
-                this->ui->SHMcheckBox->setChecked(false);
-                this->ui->UDPcheckBox->setChecked(false);
-                this->ui->TCPcheckBox->setChecked(false);
-                m_options->m_intraprocess_transport = (false);
-                m_options->m_datasharing_transport = (false);
-                m_options->m_shm_transport = (false);
-                m_options->m_udp_transport = (false);
-                m_options->m_tcp_transport = (false);
-
-                this->ui->lossSpin->setEnabled(true);
-                this->ui->label_7->setEnabled(true);
-                m_options->m_lossSampleEnabled = true;
-                mp_sd->setOptions(*m_options);
-            }
-            break;
-            case QMessageBox::Cancel:
-            {
-                this->ui->lossCheckBox->setCheckState(Qt::Unchecked);
-                m_options->m_lossSampleEnabled = false;
-            }
-            break;
-            default:
-                // should never be reached
-                break;
-        }
-    }
-    else
-    {
-        this->ui->IntraprocesscheckBox->setEnabled(true);
-        this->ui->DataSharingcheckBox->setEnabled(true);
-        this->ui->SHMcheckBox->setEnabled(true);
-        this->ui->UDPcheckBox->setEnabled(true);
-        this->ui->TCPcheckBox->setEnabled(true);
-        this->ui->IntraprocesscheckBox->setChecked(true);
-        this->ui->DataSharingcheckBox->setChecked(true);
-        this->ui->SHMcheckBox->setChecked(true);
-        this->ui->UDPcheckBox->setChecked(true);
-        this->ui->TCPcheckBox->setChecked(false);
-        m_options->m_intraprocess_transport = (true);
-        m_options->m_datasharing_transport = (true);
-        m_options->m_shm_transport = (true);
-        m_options->m_udp_transport = (true);
-        m_options->m_tcp_transport = (false);
-
-        this->ui->lossSpin->setEnabled(arg1);
-        this->ui->label_7->setEnabled(arg1);
-        m_options->m_lossSampleEnabled = false;
-        mp_sd->setOptions(*m_options);
-    }
-}
->>>>>>> a002fdb (Add an auto fill type information check (#107))
