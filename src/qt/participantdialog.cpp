c8a3f614eb57371cb0e1dbec4360802f239d7d91
JesusPoderoso <jesuspoderoso@eprosima.com>
2024-02-13 16:48:03 +0100

    Refs #20410: Remove Data-Sharing option from ShapesDemo
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- src/qt/participantdialog.cpp
+++ src/qt/participantdialog.cpp
@@ -42,7 +42,6 @@ ParticipantDialog::ParticipantDialog(
 
     // Transport Configurations
     this->ui->IntraprocesscheckBox->setChecked(m_options->m_intraprocess_transport);
-    this->ui->DataSharingcheckBox->setChecked(m_options->m_datasharing_transport);
     this->ui->SHMcheckBox->setChecked(m_options->m_shm_transport);
     this->ui->UDPcheckBox->setChecked(m_options->m_udp_transport);
     this->ui->TCPcheckBox->setChecked(m_options->m_tcp_transport);
@@ -102,7 +101,6 @@ void ParticipantDialog::setEnableState()
     }
 #endif // ifdef ENABLE_ROS_COMPONENTS
     this->ui->IntraprocesscheckBox->setEnabled(mb_started);
-    this->ui->DataSharingcheckBox->setEnabled(mb_started);
     this->ui->SHMcheckBox->setEnabled(mb_started);
     this->ui->UDPcheckBox->setEnabled(mb_started);
     this->ui->TCPcheckBox->setEnabled(mb_started);
@@ -221,13 +219,6 @@ void ParticipantDialog::on_IntraprocesscheckBox_stateChanged(
     mp_sd->setOptions(*m_options);
 }
 
-void ParticipantDialog::on_DataSharingcheckBox_stateChanged(
-        int arg1)
-{
-    m_options->m_datasharing_transport = arg1;
-    mp_sd->setOptions(*m_options);
-}
-
 void ParticipantDialog::on_SHMcheckBox_stateChanged(
         int arg1)
 {
@@ -297,17 +288,14 @@ void ParticipantDialog::on_lossCheckBox_stateChanged(
             {
                 // Transport Configurations
                 this->ui->IntraprocesscheckBox->setEnabled(false);
-                this->ui->DataSharingcheckBox->setEnabled(false);
                 this->ui->SHMcheckBox->setEnabled(false);
                 this->ui->UDPcheckBox->setEnabled(false);
                 this->ui->TCPcheckBox->setEnabled(false);
                 this->ui->IntraprocesscheckBox->setChecked(false);
-                this->ui->DataSharingcheckBox->setChecked(false);
                 this->ui->SHMcheckBox->setChecked(false);
                 this->ui->UDPcheckBox->setChecked(false);
                 this->ui->TCPcheckBox->setChecked(false);
                 m_options->m_intraprocess_transport = (false);
-                m_options->m_datasharing_transport = (false);
                 m_options->m_shm_transport = (false);
                 m_options->m_udp_transport = (false);
                 m_options->m_tcp_transport = (false);
@@ -332,17 +320,14 @@ void ParticipantDialog::on_lossCheckBox_stateChanged(
     else
     {
         this->ui->IntraprocesscheckBox->setEnabled(true);
-        this->ui->DataSharingcheckBox->setEnabled(true);
         this->ui->SHMcheckBox->setEnabled(true);
         this->ui->UDPcheckBox->setEnabled(true);
         this->ui->TCPcheckBox->setEnabled(true);
         this->ui->IntraprocesscheckBox->setChecked(true);
-        this->ui->DataSharingcheckBox->setChecked(true);
         this->ui->SHMcheckBox->setChecked(true);
         this->ui->UDPcheckBox->setChecked(true);
         this->ui->TCPcheckBox->setChecked(false);
         m_options->m_intraprocess_transport = (true);
-        m_options->m_datasharing_transport = (true);
         m_options->m_shm_transport = (true);
         m_options->m_udp_transport = (true);
         m_options->m_tcp_transport = (false);
